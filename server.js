eb53581abad66ed23f7c909633129a911b0f491e
Yagub bc2025-6 <aliev092006@gmail.com>
2025-11-17 16:58:28 +0200

    feat(cli): add commander, superagent, nodemon setup and CLI args handling

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("Server started. CLI arguments will be handled here.");
+import { program } from "commander";
+
+program
+  .requiredOption("--host <host>", "Server host")
+  .requiredOption("--port <port>", "Server port")
+  .requiredOption("--cache <dir>", "Cache directory");
+
+program.parse(process.argv);
+
+const options = program.opts();
+console.log("CLI arguments:", options);
